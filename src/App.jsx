a0c171df1e4df97b17dcaf6defd4077d64776509
anthony-gomez21 <124420854+anthony-gomez21@users.noreply.github.com>
2023-11-06 22:45:01 -0300

    Instalacion de Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './components/compartidos/Footer';
+import Header from './components/compartidos/Header';
+import Main from './components/compartidos/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/compartidos/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="text-red-500">
+      <small>Desarrollado por Anthony Gomez. @2023 </small>
+    </footer>
+  );
+};
+
+export default Footer;
